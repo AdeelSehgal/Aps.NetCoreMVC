5caeaed827f38e0b512c89fdb2d9a25e02c2e4a4
adeel <adeelsehgal83@gmail.com>
2023-07-09 17:26:46 +0500

    Added MVC to application

---
+++ MyBookStore/MyBookStore/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBookStore.Controllers
+{
+	public class HomeController:Controller
+	{
+		public string Index()
+		{
+			return "Hello Adeel";
+		}
+	}
+}
